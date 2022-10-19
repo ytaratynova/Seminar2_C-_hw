fcf11c2765a4131c985cfea636bdddc04106a972
Iuliia <ytaratynova@gmail.com>
2022-10-19 17:36:44 +0300

    Task3 orrected

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,39 +1,17 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
 Console.Clear();
 
-Console.Write("Введите координату Х точки: ");
-int x = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Задайте номер четверти от 1 до 4: ");
+int chetvert = int.Parse(Console.ReadLine() ?? "0");
 
-Console.Write("Введите координату Y точки: ");
-int y = int.Parse(Console.ReadLine() ?? "0");
-
-if (x == 0 | y == 0) 
+if (chetvert > 0 && chetvert < 5) 
 {
-    // Если точка находится на одной из осей координат или на пересечении осей координат при (0,0):
-    Console.WriteLine("Точка находится на оси координат");
-    return;
+    if (chetvert == 1) Console.WriteLine("Координаты по оси X (0, +N), координаты по оси Y (0, +N)");
+    if (chetvert == 2) Console.WriteLine("Координаты по оси X (0, -N), координаты по оси Y (0, +N)");
+    if (chetvert == 3) Console.WriteLine("Координаты по оси X (0, -N), координаты по оси Y (0, -N)");
+    if (chetvert == 4) Console.WriteLine("Координаты по оси X (0, +N), координаты по оси Y (0, -N)");
 }
-
-if (x > 0)
+else
 {
-    if (y > 0)
-    {
-        Console.WriteLine("Точка находится в первой четверти");
-    }
-    else
-    {   
-        Console.WriteLine("Точка находится в четвертой четверти");
-    }
-}
-
-if (x < 0)
-{
-    if (y > 0)
-    {
-        Console.WriteLine("Точка находится во второй четверти");
-    }
-    else
-    {   
-        Console.WriteLine("Точка находится в третьей четверти");
-    }
+    Console.WriteLine("Номер четверти введен некорректно");
 }
